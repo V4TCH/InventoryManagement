bf809a4a7ba14ad5716680858d6cb52f3b96ced7
The Vatch <thevatch@outlook.com>
2018-11-06 15:38:35 +0000

    App contains Firebase errors, syncing for later development

--- app/src/main/java/com/universityprojects/v4tch/inventorymanagement/LoginActivity.java
+++ app/src/main/java/com/universityprojects/v4tch/inventorymanagement/LoginActivity.java
@@ -1,21 +1,41 @@
 package com.universityprojects.v4tch.inventorymanagement;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.EditText;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth.getInstance();
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+    public void signIn() {
+        EditText email = R.id.email_text;
+        EditText password = R.id.passw_text;
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "signInWithEmail:success");
 
-    private void firebaseAuth() {
-        EditText email = findViewById(R.id.email_text);
-        EditText passw = findViewById(R.id.passw_text);
-
+            }
+        })
+        }
     }
 }
