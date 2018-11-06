f1a7da734c117c1ad970af750e93c55ae107efd5
V4TCH <thevatch@outlook.com>
2018-11-06 19:40:29 +0000

    Adding Firebase sign up and sign in auth

--- app/src/main/java/com/universityprojects/v4tch/inventorymanagement/LoginActivity.java
+++ app/src/main/java/com/universityprojects/v4tch/inventorymanagement/LoginActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -11,31 +12,31 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "EmailPassword";
+
+    private EditText mEmailText;
+    private EditText mPasswordText;
+
+    // add view by ID here ..................................
+
     private FirebaseAuth mAuth;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth.getInstance();
-    }
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-    }
-    public void signIn() {
-        EditText email = R.id.email_text;
-        EditText password = R.id.passw_text;
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Log.d(TAG, "signInWithEmail:success");
 
-            }
-        })
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.btn_login) {
+            // complete soon pls
         }
     }
 }
