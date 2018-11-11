6267844206b5bd3131b0db7b1c092e8fa25b5d57
The Vatch <thevatch@outlook.com>
2018-11-11 22:18:11 +0000

    Added create account method

--- app/src/main/java/com/universityprojects/v4tch/inventorymanagement/SignUpActivity.java
+++ app/src/main/java/com/universityprojects/v4tch/inventorymanagement/SignUpActivity.java
@@ -1,11 +1,17 @@
 package com.universityprojects.v4tch.inventorymanagement;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "emailPassword";
@@ -18,6 +24,20 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     private void createAccount(String email, String password) {
         Log.d(TAG, "createAccount" + email);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail: Success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure");
+                            Toast.makeText(SignUpActivity.this, "Authentication faiuled", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     @Override
